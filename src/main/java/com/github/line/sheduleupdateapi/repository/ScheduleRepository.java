1ffbf96baeccf2044be1c60bc9b042fb70002c92
creazy <mlodyv111@gmail.com>
2020-12-07 12:31:45 +0000

    Fixed query with @Modyfing

--- src/main/java/com/github/line/sheduleupdateapi/repository/ScheduleRepository.java
+++ src/main/java/com/github/line/sheduleupdateapi/repository/ScheduleRepository.java
@@ -2,11 +2,13 @@ package com.github.line.sheduleupdateapi.repository;
 
 import com.github.line.sheduleupdateapi.domain.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    @Query("UPDATE Schedule AS s SET s.isLatest = false")
+    @Modifying
+    @Query("UPDATE Schedule AS s SET s.isLatest = false WHERE s.isLatest = true")
     void deprecateLatestSchedule();
 }
